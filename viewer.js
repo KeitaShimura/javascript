e2e843589f126b1e59c54a89dfc8c9b2bed0fbd9
KeitaShimura <keitashimura2023@gmail.com>
2023-02-02 21:39:46 +0900

    lesson7

--- viewer.js
+++ viewer.js
@@ -4,11 +4,23 @@ function revBtnClick() {
     picNo--;
     document.getElementById('no').innerHTML = 'No.' + picNo;
     document.getElementById('pict').src = 'img/' + picNo + '.jpg';
-    document.getElementById('no').className = 'no' + picNo;
+
+    if (picNo === 1) {
+        document.getElementById('rev_btn').disabled = 'disabled';
+    } else if (picNo === 3) {
+        document.getElementById('fwd_btn').disabled = false;
+        document.getElementById('no').className = 'no1';
+    }
 }
 function fwdBtnClick() {
     picNo++;
     document.getElementById('no').innerHTML = 'No.' + picNo;
     document.getElementById('pict').src = 'img/' + picNo + '.jpg';
-    document.getElementById('no').className = 'no' + picNo;
+
+    if (picNo === 4) {
+        document.getElementById('no').className = 'no2';
+        document.getElementById('fwd_btn').disabled = 'disabled';
+    } else if (picNo === 2) {
+        document.getElementById('rev_btn').disabled = false;
+    }
 }
